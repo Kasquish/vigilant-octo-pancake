f151d4fe10427566a7df863c7aa59e0c91a0e600
rrrobinsonUI <rrrobnson@uiowa.edu>
2019-01-03 19:41:56 -0600

    Added init and setup.

--- commands/testCommand.py
+++ commands/testCommand.py
@@ -1,6 +1,18 @@
 import discord
 from discord.ext import commands
 
-@commands.command(pass_context = True)
-async def crazyTestCommand(ctx,*args):
-    await ctx.send("Pants!")
+
+class testCommandCog:
+    
+
+    def __init__(self, bot):
+            self.bot = bot
+
+
+    @commands.command(pass_context = True)
+    async def crazyTestCommand(ctx,*args):
+        await bot.say("Pants!")
+
+
+def setup(bot):
+    bot.add_cog(MembersCog(bot))
